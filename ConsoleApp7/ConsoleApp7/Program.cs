9240844b9dea08642fbce7add87b136bbf28f91a
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-08-06 23:41:48 +0400

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string Sentence = "bu fenn maraqlidir";
+            Console.WriteLine( "sentence contains {0} spaces ", Countspaces(spaces));
+        }
+        public static int  Countspaces (string str)
+        {
+
+            int spaces = 0;
+            string strone;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str == " ")
+                    spaces++;
+                }
+                return spaces;
+
+            }
+        
+      
+    }
+}
